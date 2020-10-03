9063e01bf615b84f51f8d7a496409043997b045a
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-03 13:44:09 -0500

    updated pseudocode and input time to header

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
-// define global variables
+
 //create fx that will input times into first col of the table from moment needs to repeat for every row
+    //Need to find amount of hours
+    //Create a loop to input time into time column/ind.row
+    //if then statement for past/present, probably input an interval timer here to update appearance of calendar
 //create text input fx that will save to local storage in middle col  needs to repeat for every row
 //create save btn  needs to repeat for every row
 //empty row needs to be gray if time past, green if free, red if busy (toggle fx?)
+
+//states current date and puts in header
+$("#currentDay").text(moment().format("[Today is ]dddd, MMMM Do"));

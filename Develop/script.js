31c59bb437c83bcd01a3e8edcd1e2cb846d8bbf1
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-03 12:02:38 -0500

    added pseudocode and basic structure in html, and added pseudocode in javascript file

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+// define global variables
+//create fx that will input times into first col of the table from moment needs to repeat for every row
+//create text input fx that will save to local storage in middle col  needs to repeat for every row
+//create save btn  needs to repeat for every row
+//empty row needs to be gray if time past, green if free, red if busy (toggle fx?)

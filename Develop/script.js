37939e91fef195e9e10791f9a0a432c81db396fb
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-04 10:53:41 -0500

    was able to get task input area and save button to save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,29 @@ $(document).ready(function(){
         loadSettings();
     });
   //states current date and puts in header
-$("#currentDay").text(moment().format("LLLL"));
+    $("#currentDay").text(moment().format("LLLL"));
 
-    // //set items in local storage.
+    // //set items in local storage. resource:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+        var inputdata = Object.keys(localStorage);
+        for (let i = 0; i < inputdata.length; i++) {
+            var userinput = localStorage.getItem(inputdata[i]);
+            var savetostorage = $("#" + inputdata[i]).find("textarea")
+            savetostorage.val(userinput);
+        }
+        //stop save button from submitting form to save data to text input area
+        $(".saveBtn").on("click", function (e) {
+            e.preventDefault();
+            // console.log(this);
+            var userinput = $(this).siblings(".taskdata").val();
+            var savetostorage = $(this).parent().attr("id");
+    
+            localStorage.setItem(savetostorage, userinput);
+        });
 
-
+        
+    
+    
+    });
 //create fx that will input times into first col of the table from moment needs to repeat for every row
     //Color coding schedule dependant upon hour
     //Need to find amount of hours
@@ -22,6 +40,6 @@ $("#currentDay").text(moment().format("LLLL"));
 
     // //set items in local storage.
     // localStorage.setItem(time, text);
-});//empty row needs to be gray if time past, green if free, red if busy (toggle fx?)
+//empty row needs to be gray if time past, green if free, red if busy (toggle fx?)
     //Color coding schedule dependant upon hour
 
